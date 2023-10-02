0ceef0809a95d2b9651657249c23794234d5b041
Abdrlrazzaq Asfopur <130183951+Abdelrazzaq-Asfour@users.noreply.github.com>
2023-10-02 19:10:14 +0300

    mJS

--- src/main.js
+++ src/main.js
@@ -1,60 +1,125 @@
 import CardComp from './card';
+
 import './main.css';
+
 import { useEffect, useState } from 'react';
+
 import Form from 'react-bootstrap/Form';
+
 import Button from 'react-bootstrap/Button';
 
+ 
+
 function Main (){
+
     let [items, setItems] = useState([]);
+
+ 
 
    async function getData(){
 
-    const url = 'www.themealdb.com/api/json/v1/1/search.php?f=a';
-    const options = {
-      method: 'GET',
-      headers: {
-       // 'X-RapidAPI-Key': '0f2abd4395msh6ce1c28ccac1aacp178ddbjsn9cd63c10ba80',
-        //'X-RapidAPI-Host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
-      }
-    };
-    
+ 
+
+    const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
+
+    // const options = {
+
+    //   method: 'GET', 
+
+    //   headers: {
+
+    //    // 'X-RapidAPI-Key': '0f2abd4395msh6ce1c28ccac1aacp178ddbjsn9cd63c10ba80',
+
+    //     //'X-RapidAPI-Host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
+
+    //   }
+
+    // };
+
+   
+
     try {
-      const response = await fetch(url, options);
+
+      const response = await fetch(url);
+
       const result = await response.json();
+
       setItems(result.meals)
+
     } catch (error) {
+
       console.error(error);
+
     }
+
 }
+
+ 
 
 useEffect(function (){getData()}, [])
 
  
+
+ 
+
     return(
+
         <>
+
           <Form className="d-flex"  id="myform">
+
             <Form.Control
+
               type="search"
+
               placeholder="Search"
+
               className="me-2"
+
               aria-label="Search"
+
               name="search"
+
               />
+
             <Button variant="outline-success" type='submit'>Search</Button>
+
           </Form>
+
         <div id="container">
+
         {items.map(function(item){
+
             return(
+
               <>
-              <CardComp image={item.strMealThumb[0]} title={item.strMeal} description= {item.strInstructions} price={item.price} />
+
+              <CardComp image={item.strMealThumb}
+
+              title={item.strMeal}
+
+              description={item.strInstructions}
+
+              Category={item.strCategory} />
+
                 </>
+
             )
+
         }
+
     )
+
         }
+
         </div>
+
         </>
+
     )
+
 }
+
+ 
 
 export default Main;
