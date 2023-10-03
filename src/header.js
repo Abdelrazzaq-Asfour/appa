86685b92386550943b7d39a425f26e7aee69801e
Abdrlrazzaq Asfopur <130183951+Abdelrazzaq-Asfour@users.noreply.github.com>
2023-10-03 07:40:40 +0300

    lab6

--- src/header.js
+++ src/header.js
@@ -8,12 +8,12 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container >
-        <Navbar.Brand href="">My Title</Navbar.Brand>
+        <Navbar.Brand href="">Food Recipes</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/products">Products</Nav.Link>
+            <Nav.Link href="/products">Meun</Nav.Link>
             <Nav.Link href="/favorites">Favorites</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
